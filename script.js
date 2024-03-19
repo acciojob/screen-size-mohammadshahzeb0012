985f7d36b694e0c28385cf588d8ba915148107ee
Mohammad Shahzeb <113281464+mohammadshahzeb0012@users.noreply.github.com>
2024-03-19 13:13:00 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const sizeInfo =  document.getElementById('sizeInfo')
+let width = window.screen.width
+let height = window.screen.height
+let h1 = document.createElement('h1');
+h1.innerText = `Width: ${width} and Height: ${height}`
+sizeInfo.append(h1)
